6bac3a3ad64f613d427440e3ba322b8d606ada2f
Lars Ree <70200269+lars-ree@users.noreply.github.com>
2022-06-20 14:09:35 +0200

    some stuff

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 function SearchPhotos() {
     let clientId = "sYoUMawzOY5bv-nXDaU-zLmM1Hb6LVW2Xv_B2epudkU";
+
     let query = document.getElementById("search").value;
-    let url = "https://api.unsplash.com/search/photos/?client_id=" + clientId + "&query" + query;
+
+    let url = "https://api.unsplash.com/search/photos/?client_id="+clientId+"&query"+ query;
 
 
     fetch(url)
     .then(function(data){
-        console.log(data);
+        return data.json();
     })
-}
+    .then(function (data){
+        console.log(data);
+
+        data.results.forEach(photo => {
+            let result = `
+            <img src="${photo.urls.regular}">
+            `;
+            document.getElementById("result").append(result)
+            
+        });
+    })
+};
+
+
