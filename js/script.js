b6b11bd43b197fc3c8e94e7c1417808f4fcdda02
lars-ree <70200269+lars-ree@users.noreply.github.com>
2022-06-24 10:49:29 +0200

    some search shit

--- js/script.js
+++ js/script.js
@@ -1,93 +1,84 @@
-const access_key = 'sYoUMawzOY5bv-nXDaU-zLmM1Hb6LVW2Xv_B2epudkU';
+const access_key = "sYoUMawzOY5bv-nXDaU-zLmM1Hb6LVW2Xv_B2epudkU";
 
 const random_photo_url = `https://api.unsplash.com/photos/random?client_id=${access_key}&count=30`;
-
 
 let allImages; // this will store all the images
 
 const getImages = () => {
-    fetch(random_photo_url)
-    .then(res => res.json())
-    .then(data => {
-        allImages = data;
-        makeImages(allImages);
+  fetch(random_photo_url)
+    .then((res) => res.json())
+    .then((data) => {
+      allImages = data;
+      makeImages(allImages);
     });
-}
+};
 
-
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
 function makeImages(data) {
-    data.forEach((item, index) => {
+  data.forEach((item, index) => {
+    let img = document.createElement("img");
+    img.src = item.urls.regular;
+    img.className = "gallery-img";
 
-        let img = document.createElement('img');
-        img.src = item.urls.regular;
-        img.className = 'gallery-img';
-
-        gallery.appendChild(img);
-
-    });
+    gallery.appendChild(img);
+  });
 }
 
-getImages()
-
+getImages();
 
 let currentImage = 0; // will track the current large image
 
-
-
 const showPopup = (item) => {
-    let popup = document.querySelector('.image-popup');
-    const downloadBtn = document.querySelector('.download-btn');
-    const closeBtn = document.querySelector('.close-btn');
-    const image = document.querySelector('.large-img');
+  let popup = document.querySelector(".image-popup");
+  const downloadBtn = document.querySelector(".download-btn");
+  const closeBtn = document.querySelector(".close-btn");
+  const image = document.querySelector(".large-img");
 
-    popup.classList.remove('hide');
-    downloadBtn.href = item.links.html;
-    image.src = item.urls.regular;
+  popup.classList.remove("hide");
+  downloadBtn.href = item.links.html;
+  image.src = item.urls.regular;
 
-    closeBtn.addEventListener('click', () => {
-        popup.classList.add('hide');
-    })
-
-}
+  closeBtn.addEventListener("click", () => {
+    popup.classList.add("hide");
+  });
+};
 
 // controls
 
-const preBtns = document.querySelector('.pre-btn');
-const nxtBtns = document.querySelector('.nxt-btn');
+const preBtns = document.querySelector(".pre-btn");
+const nxtBtns = document.querySelector(".nxt-btn");
 
-preBtns.addEventListener('click', () => {
-    if(currentImage > 0){
-        currentImage--;
-        showPopup(allImages[currentImage]);
-    }
-})
+preBtns.addEventListener("click", () => {
+  if (currentImage > 0) {
+    currentImage--;
+    showPopup(allImages[currentImage]);
+  }
+});
 
-nxtBtns.addEventListener('click', () => {
-    if(currentImage < allImages.length - 1){
-        currentImage++;
-        showPopup(allImages[currentImage]);
-    }
-})
+nxtBtns.addEventListener("click", () => {
+  if (currentImage < allImages.length - 1) {
+    currentImage++;
+    showPopup(allImages[currentImage]);
+  }
+});
 
-let searchParam = location.search.split('=').pop(); // this will give extract the search keyword from URL
+let searchParam = location.search.split("=").pop(); // this will give extract the search keyword from URL
 
 const search_photo_url = `https://api.unsplash.com/search/photos?client_id=${access_key}&query=${searchParam}&per_page=50`; // this is search image URL
 
-
 const searchImages = () => {
-    fetch(search_photo_url)
-    .then(res => res.json())
-    .then(data => {
-        allImages = data.results;
-        makeImages(allImages);
-       
+  fetch(search_photo_url)
+    .then((res) => res.json())
+    .then((data) => {
+      allImages = data.results;
+      makeImages(allImages);
+      console.log(data);
     });
-}
+};
 
-if(searchParam == ''){
-    getImages();
-} else{
-    searchImages();
+if (searchParam == "") {
+  getImages();
+} else {
+  searchImages();
 }
